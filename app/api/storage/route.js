e361a266bae2dfe10abf273c7be74b98148f9f87
ianm1837 <ian@iwm.dev>
2023-07-06 20:52:13 -0700

    created api for saving server data

---
+++ app/api/storage/route.js
@@ -0,0 +1,93 @@
+const { PrismaClient } = require('@prisma/client')
+import { NextResponse, NextRequest } from 'next/server'
+import { headers } from 'next/headers'
+
+const prisma = new PrismaClient()
+
+
+export async function GET() {
+
+  //get all servers
+  const allServers = await prisma.servers.findMany()
+
+  return NextResponse.json(allServers)
+}
+
+
+export async function POST(request) {
+  //for adding new data
+
+  const { serverName, serverAddress, lastDisabledTime } = await request.json()
+
+  try{
+    const createdObject = await prisma.servers.create({
+      data:{
+        serverName: serverName,
+        serverAddress: serverAddress
+      }
+    })
+    return NextResponse.json(createdObject)
+  } catch(error) {
+    //find type of error
+    console.log("Error Type: ", error.constructor.name)
+
+    //find conflicting field(s)
+    console.log("Error Target: ", error.meta.target)
+
+    //find verbose error message
+    console.log("Error Message: ", error.message)
+
+    //find error code
+    console.log("Error Code", error.code)
+    
+    return NextResponse.json({
+      errorCode: error.code, 
+      errorMessage: error.message, 
+      errorTarget: error.meta.target
+    })
+}
+  
+}
+
+export async function PUT(reuqest) {
+  //for updating current data
+
+  //get json body
+  const requestJSON = await reuqest.json()
+
+  //update the database based on the values provided
+  try{
+    const updatedRecord = await prisma.servers.update({
+      where: {
+        id: requestJSON.id
+      },
+      data: {
+        serverName: requestJSON.serverName,
+        serverAddress: requestJSON.serverAddress
+      }
+    })
+      return NextResponse.json(updatedRecord)
+  }catch{ (error) => {
+    return NextResponse.json({
+      errorCode: error.code, 
+      errorMessage: error.message, 
+      errorTarget: error.meta.target
+    })
+  }
+  }
+}
+
+export async function DELETE(request){
+  //for deleting the data
+  // const params = new URL(request)
+  // console.log(await request.nextUrl.searchParams.get("id"))
+  const requestJSON = await request.json()
+
+  const deletedValue = await prisma.servers.delete({
+    where: {
+      id: requestJSON.id
+    }
+  })
+
+  return NextResponse.json(deletedValue)
+}
---
+++ prisma/migrations/20230706010109_v2/migration.sql
@@ -0,0 +1,15 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Servers" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "serverName" TEXT NOT NULL,
+    "serverAddress" TEXT NOT NULL,
+    "lastDisabledTime" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+INSERT INTO "new_Servers" ("id", "lastDisabledTime", "serverAddress", "serverName") SELECT "id", "lastDisabledTime", "serverAddress", "serverName" FROM "Servers";
+DROP TABLE "Servers";
+ALTER TABLE "new_Servers" RENAME TO "Servers";
+CREATE UNIQUE INDEX "Servers_serverName_key" ON "Servers"("serverName");
+CREATE UNIQUE INDEX "Servers_serverAddress_key" ON "Servers"("serverAddress");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;

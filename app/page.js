b352310a3653acad6f3a2ccc959c695b0efd388a
ianm1837 <ian@iwm.dev>
2023-08-22 20:20:54 -0700

    last commit for modal; changing design

--- app/page.js
+++ app/page.js
@@ -71,18 +71,66 @@ export default async function Home() {
       {/* modal */}
       <dialog id="my_modal_1" className="modal">
         <form method="dialog" className="modal-box">
-          <h3 className="font-bold text-lg">Add Server</h3>
-          <form>
-            <label>
-              Server Name
-            </label>
+          <h1 className="font-bold text-lg">Add Server</h1>
 
-            <input />
+          <div className="form-control w-full max-w-xs">
+
+            <label for="serverName">
+              <span className="label-text">Server Name</span>
+            </label>
+            <input id="serverName" type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs mb-2" />
+
+            <label for="serverIP">
+              <span className="label-text">Server IP</span>
+            </label>
+            <input id="serverIP" type="text" placeholder="Type here" className="input input-bordered input-primary w-full max-w-xs mb-2" />
+
+
             
+          </div>
             <h3  className="font-bold text-lg">
               Current Servers
-            </h3>
-          </form>
+          </h3>
+          <table className="table w-full max-w-xs">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>IP</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* row 1 */}
+              <tr>
+                <th>1</th>
+                <td>PiHole Alpha</td>
+                <td>
+                  10.0.0.0
+                </td>
+                <td>
+                  <button className="btn btn-sm btn-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/></svg>
+                  </button>
+                </td>
+              </tr>
+              {/* row 2 */}
+              <tr>
+                <th>2</th>
+                <td>PiHole Beta</td>
+                <td>
+                  10.0.0.0
+                </td>
+                <td>
+                  <button className="btn btn-sm btn-error">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V9c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v10zM18 4h-2.5l-.71-.71c-.18-.18-.44-.29-.7-.29H9.91c-.26 0-.52.11-.7.29L8.5 4H6c-.55 0-1 .45-1 1s.45 1 1 1h12c.55 0 1-.45 1-1s-.45-1-1-1z"/></svg>
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+
+
           <div className="modal-action">
             <SaveSettingsButton />
           </div>
